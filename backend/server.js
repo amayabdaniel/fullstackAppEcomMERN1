d9e9089427ae526214d5e4ff1706eaeda1f4ced6
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-05-27 17:19:45 -0500

    [ADD] routes1

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const dotenv = require('dotenv')
 
 // Setting config file
-dotenv.config({path: 'server/config/config.env'})
+dotenv.config({path: 'backend/config/config.env'})
 
 app.listen(process.env.PORT, () => {
     console.log(`server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
